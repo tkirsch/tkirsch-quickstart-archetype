bdb8ed54336c7e8efd2e35ffade181f5c9b4068c
Thomas Kirsch <tkirsch79@googlemail.com>
2016-08-05 20:42:53 +0200

    extend controller example; add form error example

--- src/main/resources/archetype-resources/src/main/java/controller/AppController.java
+++ src/main/resources/archetype-resources/src/main/java/controller/AppController.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,7 +20,7 @@ import ${package}.domain.AppRepository;
 @Controller
 @RequestMapping("/apps")
 public class AppController {
-	
+
 	private static final transient Logger LOG = LoggerFactory.getLogger(AppController.class);
 	@Inject
 	private AppRepository appRepository;
@@ -32,9 +33,12 @@ public class AppController {
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = "/{id}")
-	public String show(@PathVariable final Long id, final Model model) {
+	public String show(@PathVariable final Integer id, final Model model) {
 		LOG.debug("show");
 		App app = appRepository.findById(id);
+		if (app == null) {
+			return "redirect:/404.html";
+		}
 		model.addAttribute("app", app);
 		return "app/show";
 	}
@@ -47,34 +51,57 @@ public class AppController {
 		model.addAttribute("app", app);
 		return "app/new";
 	}
-	
+
 	@RequestMapping(method = RequestMethod.GET, value = "/{id}/edit")
-	public String edit(@PathVariable final Long id, final Model model) {
+	public String edit(@PathVariable final Integer id, final Model model) {
 		LOG.debug("edit");
 		App app = appRepository.findById(id);
+		if (app == null) {
+			return "redirect:/404.html";
+		}
 		model.addAttribute("app", app);
 		return "app/edit";
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public String save(final App app) {
+	public String save(final App newApp, final BindingResult result) {
 		LOG.debug("save");
-		appRepository.save(app);
+		if (appRepository.findByName(newApp.getName()) != null) {
+			result.rejectValue("name", "app.error.alreadyexits.name");
+		}
+		if (result.hasErrors()) {
+			return "app/new";
+		}
+		appRepository.save(newApp);
 		return "redirect:/apps";
 	}
 
 	@RequestMapping(method = RequestMethod.PUT, value = "/{id}")
-	public String update(@PathVariable final Long id, final App updatedApp) {
+	public String update(@PathVariable final Integer id, final App updatedApp, final BindingResult result) {
 		LOG.debug("update");
-		App app = appRepository.findById(id);
-		app.setName(updatedApp.getName());
-		appRepository.save(app);
+		App existingApp = appRepository.findById(id);
+		if (existingApp == null) {
+			return "redirect:/404.html";
+		}
+		App byName = appRepository.findByName(updatedApp.getName());
+		if ((byName != null) && !byName.getId().equals(id)) {
+			result.rejectValue("name", "app.error.alreadyexits.name");
+		}
+		if (result.hasErrors()) {
+			return "app/edit";
+		}
+		existingApp.setName(updatedApp.getName());
+		appRepository.save(existingApp);
 		return "redirect:/apps";
 	}
-	
+
 	@RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
-	public String delete(@PathVariable final Long id) {
+	public String delete(@PathVariable final Integer id) {
 		LOG.debug("delete");
+		App existingApp = appRepository.findById(id);
+		if (existingApp == null) {
+			return "redirect:/404.html";
+		}
 		appRepository.remove(id);
 		return "redirect:/apps";
 	}
--- src/main/resources/archetype-resources/src/main/java/domain/App.java
+++ src/main/resources/archetype-resources/src/main/java/domain/App.java
@@ -13,31 +13,39 @@ import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "APP")
-@NamedQueries({ @NamedQuery(name = App.QUERY_NAME_FIND_ALL, query = "select e from App e") })
+@NamedQueries({ @NamedQuery(name = App.QUERY_NAME_FIND_ALL, query = "select e from App e"),
+		@NamedQuery(name = App.QUERY_NAME_FIND_BY_NAME, query = "select e from App e where e.name = :" + App.QUERYPARAM_NAME) })
 public class App implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
 	public static final String QUERY_NAME_FIND_ALL = "App.findAll";
+	public static final String QUERY_NAME_FIND_BY_NAME = "App.findByName";
+	public static final String QUERYPARAM_NAME = "name";
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
+	private Integer id;
+	@NotNull
+	@Size(min = 1, max = 50)
 	@Column(name = "NAME", length = 50, nullable = false, unique = true)
 	private String name;
 
 	/**
 	 * @return the id
 	 */
-	public Long getId() {
+	public Integer getId() {
 		return id;
 	}
 
 	/**
-	 * @param id the id to set
+	 * @param id
+	 *            the id to set
 	 */
-	void setId(final Long id) {
+	void setId(final Integer id) {
 		this.id = id;
 	}
 
@@ -47,9 +55,10 @@ public class App implements Serializable {
 	public String getName() {
 		return name;
 	}
-	
+
 	/**
-	 * @param name the name to set
+	 * @param name
+	 *            the name to set
 	 */
 	public void setName(final String name) {
 		this.name = name;
